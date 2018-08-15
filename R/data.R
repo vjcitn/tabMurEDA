554d571b61f9338fbcd84edab733df5be3595d57
Vince Carey <stvjc@channing.harvard.edu>
2018-08-15 15:52:01 -0400

    first draft with DelayedArray interface to quantifications at HDF Kita Lab

---
+++ R/data.R
@@ -0,0 +1,9 @@
+#' tabMurDelayed: a SummarizedExperiment instance with DelayedArray assay reference
+#' @importFrom utils data
+#' @docType data
+#' @format SummarizedExperiment
+#' @source assay data from https://github.com/czbiohub/tabula-muris-vignettes/tree/master/data (https://s3.amazonaws.com/czbiohub-tabula-muris/TM_droplet_mat.rds)
+#' @note The numerical content is provided as a DelayedArray instance, hard coded to a specific HDF Lab resource.
+#' @examples
+#' tabMurDelayed
+"tabMurDelayed"
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # tabMurEDA
+
 exploratory data analysis support for tabula muris single-cell RNA-seq data
+
+droplet counts were retrieved as rds from https://s3.amazonaws.com/czbiohub-tabula-muris/TM_droplet_mat.rds (a dgcMatrix instance), converted
+to dense format in HDF5, then uploaded to HDF Kita lab.
+
+DelayedArray infrastructure is embedded in the SummarizedExperiment
+tabMurDelayed defined in this package.
